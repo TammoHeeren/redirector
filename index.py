d33913b29c0cac7747e5d8a281f665daf6f9c50c
Tammo <tammo.heeren@gmail.com>
2019-10-17 14:22:34 -0700

    Test redirect to linkedin

--- index.py
+++ index.py
@@ -3,10 +3,8 @@ import datetime
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    return {'statusCode': 301,
+            'headers': {
+                'Location': 'https://www.linkedin.com/in/tammoheeren/'
+            }
+        }
