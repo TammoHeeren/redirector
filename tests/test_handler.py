11f6e90cbe3d027f2dff74190a41aaf9bac48b31
Tammo <tammo.heeren@gmail.com>
2019-10-17 15:36:36 -0700

    Fix test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,7 +7,7 @@ class TestHandlerCase(unittest.TestCase):
     def test_with_empty_path(self):
         result = index.handler(None, None)
         self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/html')
+        self.assertEqual(result['headers']['Content-Type'], 'text/html')
 
     def test_linkedin(self):
         result = index.handler({
