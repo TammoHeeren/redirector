2f4717eda166bd580fbd661528e6ce003da7ea81
Tammo <tammo.heeren@gmail.com>
2019-10-17 14:23:36 -0700

    Remove initial test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -5,12 +5,13 @@ import index
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
-        print("testing response.")
-        result = index.handler(None, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
+        pass
+        # print("testing response.")
+        # result = index.handler(None, None)
+        # print(result)
+        # self.assertEqual(result['statusCode'], 200)
+        # self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        # self.assertIn('Hello World', result['body'])
 
 
 if __name__ == '__main__':
